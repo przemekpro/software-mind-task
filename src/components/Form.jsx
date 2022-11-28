4652d7ddb7321335ca40c44e7af3a1fba538fe51
Przemysław Prokopczuk <przemek.prokopczuk1@gmail.com>
2022-11-28 14:13:26 +0100

    create form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,56 @@
 
 export default function Form() {
     return(
-        <h1>Form</h1>
+        <div className="form__wrapper">
+            <h1>Form</h1>
+            <form>
+                <label htmlFor="firstName">
+                    First name:
+                    <input 
+                        required
+                        name="firstName"
+                        id="firstName"
+                        type="text"
+                        placeholder="e.g. John"
+                    />
+                </label>
+
+                
+                <label htmlFor="password">
+                    Password:
+                    <input 
+                        required
+                        name="password"
+                        id="password"
+                        type="password"
+                        placeholder="*****"
+                    />
+                </label>
+
+                <label htmlFor="newsletter">
+                    Newsletter
+                    <input 
+                        name="newsletter"
+                        id="newsletter"
+                        type="checkbox"
+                        placeholder="e.g. John"
+                    />
+                </label>
+
+                <label htmlFor="email">
+                    E-mail:
+                    <input 
+                        required
+                        name="email"
+                        id="email"
+                        type="email"
+                        placeholder="e.g. john@doe.com"
+                    />
+                </label>
+
+                <button>Submit</button>
+            </form>
+        </div>
+
     )
 }
